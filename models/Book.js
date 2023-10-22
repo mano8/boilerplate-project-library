7a5c30f88a1caceb8ffb3643cc013c02555bfe7c
Eli <eli.serra173@gmail.com>
2023-10-22 05:50:48 +0200

    Add monggose book Schema

---
+++ models/Book.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    comments: [String],
+  },
+  {
+    statics: {
+      getAllBooks(done) {
+        this.find()
+        .then((books) => {
+            const result = books.map(val=>{
+                return {
+                    "_id": val._id, 
+                    "title": val.title, 
+                    "commentcount": val.comments.length }
+            })
+            done(false, result)
+        })
+        .catch((err)=>{
+          done(err)
+        });
+      },
+      findBookById(bookId, done){
+        this.findOne({"_id": bookId})
+            .then((data) => {
+              if(data !== null){
+                const result = {
+                  _id: bookId,
+                  title: data.title,
+                  comments: data.comments
+                }
+                done(false, result)
+              }else{
+                done(false, null);
+              }
+              
+            })
+            .catch((err)=>{
+              done(err)
+            });
+      },
+      addBookComment(bookId, comment, done){
+        if(comment){
+          this.findByIdAndUpdate({"_id": bookId}, {$push: {comments: comment}})
+            .then((data) => {
+              const nbComments = (data.comments.length + 1) ?? 0
+              const result = {
+                _id: bookId,
+                title: data.title,
+                commentcount: nbComments
+              }
+              done(false, result)
+            })
+            .catch((err)=>{
+              done(err)
+            });
+        }else{
+          done(false, null)
+        }
+        
+      },
+      deleteBookById(bookId, done){
+        this.deleteOne({"_id": bookId})
+          .then((data)=>{
+            done(false, data)
+          })
+          .catch((err)=>{
+            done(err)
+          })
+      },
+      deleteAllBooks(done){
+        this.deleteMany()
+          .then((data)=>{
+            done(false, data)
+          })
+          .catch((err)=>{
+            done(err)
+          })
+      }
+    }
+    
+  });
+
+
+let Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
